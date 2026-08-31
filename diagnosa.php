09db5b047b447aea65241015adf3fc88b8399efa
Khaidir <man.tigabjr@gmail.com>
2026-08-31 17:34:55 +0800

    Tambah diagnosa.php untuk lacak error

---
+++ diagnosa.php
@@ -0,0 +1,8 @@
+<?php
+// File diagnosa cepat untuk mengetahui error CodeIgniter
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+define('ENVIRONMENT', 'development');
+
+require_once __DIR__ . '/index.php';
